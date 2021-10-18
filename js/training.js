837cdf48382a9b7d6aad4ab486cddeb293a58962
Valerii Kurochka <78504019+Va110k@users.noreply.github.com>
2021-10-18 22:03:44 +0300

    12323

--- js/training.js
+++ js/training.js
@@ -137,3 +137,100 @@
 // const normalazedString = title.toLowerCase().split(" ").join("-");
 
 // console.log(normalazedString);
+
+// ex 4
+
+// const add = function (x, y) {
+//   console.log(x);
+//   console.log(y);
+//   console.log("Выполняется функция add");
+
+//   return x + y;
+// };
+
+// const r1 = add(3, 4);
+// console.log("r1:", r1);
+
+// const fn = function (value) {
+//   if (value < 50) {
+//     return "Меньше чем 50";
+//   }
+//   return "Больше чем 50";
+// };
+
+// console.log("Результат функции: ", fn(60));
+
+// const bookShelf = {
+//   authors: ["Бернард Корнуэлл", "Роберт Шекли"],
+//   getAuthors() {
+//     return this.authors;
+//   },
+//   addAuthor(authorName) {
+//     this.authors.push(authorName);
+//   },
+// };
+
+// console.log(bookShelf.getAuthors()); // ['Бернард Корнуэлл', 'Роберт Шекли']
+// bookShelf.addAuthor("Ли Танит");
+// console.log(bookShelf.getAuthors()); //
+
+//ex 6
+// const numbers = [1, 2, 3];
+
+// // Классический for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(numbers[i]);
+// }
+
+// // Функциональный forEach
+// numbers.forEach((num) => console.log(num));
+
+// // Указываем параметр idx если нужен доступ к счетчику
+// numbers.forEach((num, idx) => console.log(`index ${idx}, value ${num}`));
+
+// const users = [
+//   { id: "000", name: "Mango", isActive: true },
+//   { id: "001", name: "Poly", isActive: false },
+//   { id: "002", name: "Ajax", isActive: true },
+//   { id: "003", name: "Chelsey", isActive: false },
+// ];
+
+// // Для каждого элемента коллекции (user) проверим поле id.
+// // Если оно совпадает с искомым идентификатором, то find прекратит
+// // выполнение и вернет текущий элемент (user) как результат выполнения
+// const user = users.find((user) => user.id === "002");
+// console.log(user);
+
+// // Создадим функцию которая будет возвращать нам пользователя по id
+// const getUserById = (arr, id) => arr.find((x) => x.id === id);
+
+// console.log(getUserById(users, "001"));
+// console.log(getUserById(users, "003"));
+
+// Функция которая проверяет величину значения, возвращает true или false.
+// const isBigEnough = (val) => val >= 10;
+
+// // Допустим нам нужно узнать достаточно ли большие ВСЕ числа в коллекции.
+// // Все что нам нужно это буль true/false. Метод every вернет true только тогда,
+// // когда все элементы коллекции будут удовлетворять условию в callback-функции.
+
+// console.log([12, 5, 8, 130, 44].every(isBigEnough)); // false
+// console.log([12, 54, 18, 130, 44].every(isBigEnough)); // true
+
+// // Допустим нам нужно узнать ЕСТЬ ЛИ в коллекции числа больше 10, хотя бы одно
+// // или больше. Все что нам нужно это буль true/false. Метод some вернет true
+// // только тогда, когда хотябы 1 или более элементов коллекции будут
+// // удовлетворять условию в callback-функции.
+
+// console.log([2, 5, 8, 1, 4].some(isBigEnough)); // false
+// console.log([12, 5, 8, 1, 4].some(isBigEnough)); // true
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const even = numbers.filter((x) => x % 2 === 0);
+
+const doubled = even.map((x) => x * 2);
+
+const reversed = doubled.reverse();
+
+console.log(reversed);
